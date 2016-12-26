e9b3c17655ab5f7eaa63b5a314f6feb6de5493f8
Marco Pil <Marco.Pil@infosupport.com>
2016-12-26 02:18:02 +0100

    Initial commit

---
+++ InfoSupport.WSA.Auditlog/src/InfoSupport.WSA.Auditlog/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfoSupport.WSA.Auditlog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
